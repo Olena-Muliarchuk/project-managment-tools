290051f0ffb7299be459c222ed198493ee74120d
Olena Muliarchuk <olena.muliarchuk.r@gmail.com>
2025-07-25 22:59:10 +0300

    Fix issue with jwt_token

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -22,7 +22,7 @@ const authMiddleware = (req, res, next) => {
     const token = authHeader.split(' ')[1];
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
         req.user = decoded;
 
         logger.info('Authorization successful', {
